895bbf7bd3d1fd1d9ca23def02168a041e5bb7ee
saikiran <pallasaikiranmkl@gmail.com>
2025-06-21 19:27:44 +0530

    init commit

---
+++ src/main/java/com/sai/s3/AwsS3FileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.sai.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3FileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3FileStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/s3/AwsS3FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3FileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
